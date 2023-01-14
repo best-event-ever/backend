19e3a62f92072f6b943513fe6e8d7ede7ff441f6
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-14 11:13:09 +0100

    struktur angelegt und server.js

---
+++ app.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import router from './routes/organizerRoute.js';
+
+const URI = process.env.MONGO || "mongodb://localhost:27017/event";
+const app = express();
+const PORT = 4000;
+
+mongoose
+  .connect(URI)
+  .then(() => console.log(`Connect to Database`))
+  .catch((err) => console.log(`Failed to connect to Database`));
+
+mongoose.connection.on('error', console.log);
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server runs on Port: ${PORT}`);
+});
