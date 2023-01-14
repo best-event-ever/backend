38d36b7293b29a900370fec28bca9e722c45c011
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-14 11:33:10 +0100

    organizerRoute angelegt

--- routes/organizerRoute.js
+++ routes/organizerRoute.js
@@ -1,4 +1,15 @@
-import Express from "express";
+import express from "express";
 
 const router = express.Router();
 
+import {postOneEvent, getAllEvents, getOneEvent, deleteOneEvent, updateOneEvent} from '../controller/eventController.js';
+
+router
+  .route('/events')
+    .post(postOneEvent)
+    .get(getAllEvents)
+    .get(getOneEvent)
+    .delete(deleteOneEvent)
+    .patch(updateOneEvent);
+
+export default router;
