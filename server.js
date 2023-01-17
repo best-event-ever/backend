a13b25573337ab754a5e748609cf0342e9fddf5e
Jackeine-Matos <jackeline.matos@gmail.com>
2023-01-17 13:41:51 +0100

    backendverbindung

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
 mongoose.connection.on("error", console.log);
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(morgan("dev"));
 
 app.use("/organizer", organizerRouter);
