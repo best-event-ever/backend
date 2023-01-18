cf6a11ba475d9d00d3f16c44df91048939c34919
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-18 14:01:41 +0100

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import generalRouter from "./routes/general/generalRoute.js";
 
 const URI = process.env.MONGO || "mongodb://localhost:27017/event";
 const app = express();
-const PORT = 4017;
+const PORT = 4011;
 
 mongoose
   .connect(URI)
